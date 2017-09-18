234c2ea55c1c46f0cc1895bbd649ca27ff063cee
xinyi <xinyi.kao@titansoft.com.sg>
2017-09-18 10:39:03 +0800

    add Contains Duplicate II (Leetcode)

---
+++ Kata/Leetcode/ContainsDuplicateTwo.cs
@@ -0,0 +1,20 @@
+﻿namespace Kata.Leetcode
+{
+    public class ContainsDuplicateTwo
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = nums.Length - 1; j > 0; j--)
+                {
+                    if (nums[i] == nums[j] && i != j)
+                    {
+                        return (j - i) <= k;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ KataTest/LeetcodeTest/ContainsDuplicateTwoTest.cs
@@ -0,0 +1,53 @@
+﻿using Kata.Leetcode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTest.LeetcodeTest
+{
+    [TestClass]
+    public class ContainsDuplicateTwoTest
+    {
+        [TestMethod]
+        public void Test_ContainsDuplicateTwo_Input_Array_is_Empty_And_0_Output_Is_False()
+        {
+            var target = new ContainsDuplicateTwo();
+            var nums = new int[] { };
+            var k = 0;
+            var excepted = false;
+            var actual = target.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(excepted, actual);
+        }
+
+        [TestMethod]
+        public void Test_ContainsDuplicateTwo_Input_Array_is_123_And_0_Output_Is_False()
+        {
+            var target = new ContainsDuplicateTwo();
+            var nums = new int[] { 1, 2, 3 };
+            var k = 0;
+            var excepted = false;
+            var actual = target.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(excepted, actual);
+        }
+
+        [TestMethod]
+        public void Test_ContainsDuplicateTwo_Input_Array_is_12341_And_2_Output_Is_False()
+        {
+            var target = new ContainsDuplicateTwo();
+            var nums = new int[] { 1, 2, 3, 4, 1 };
+            var k = 2;
+            var excepted = false;
+            var actual = target.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(excepted, actual);
+        }
+
+        [TestMethod]
+        public void Test_ContainsDuplicateTwo_Input_Array_is_12341_And_5_Output_Is_True()
+        {
+            var target = new ContainsDuplicateTwo();
+            var nums = new int[] { 1, 2, 3, 4, 1 };
+            var k = 5;
+            var excepted = true;
+            var actual = target.ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}

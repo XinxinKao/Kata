e9f6b13abf174f643de91fd8b8ab8c6aa61f2728
xinyi <xinyi.kao@titansoft.com.sg>
2017-10-12 17:00:32 +0800

    add Leetcode TwoSum

---
+++ Kata/Leetcode/TwoSumSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace Kata.Leetcode
+{
+    public class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var length = nums.Length;
+
+            if (length < 2)
+            {
+                return new int[0];
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                {
+                    if (target == nums[i] + nums[j])
+                    {
+                        return new[] {i, j};
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
---
+++ KataTest/LeetcodeTest/TwoSumSolutionTest.cs
@@ -0,0 +1,23 @@
+﻿using Kata.Leetcode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace KataTest.LeetcodeTest
+{
+    [TestFixture]
+    public class TwoSumSolutionTest
+    {
+        [TestCase(9, new[] { 2, 7, 11, 5 }, new[] { 0, 1 }, TestName = "Test_TwoSum_2_7_11_5_And_Target_9_Should_Return_0_1")]
+        [TestCase(12, new[] { 2, 7, 11, 5 }, new[] { 1, 3 }, TestName = "Test_TwoSum_2_7_11_5_And_Target_12_Should_Return_1_3")]
+        [TestCase(10, new[] { 2, 7, 3, 5 }, new[] { 1, 2 }, TestName = "Test_TwoSum_2_7_3_5_And_Target_10_Should_Return_1_2")]
+        [TestCase(10, new[] { 2, 1, 3, 0 }, new int[] { }, TestName = "Test_TwoSum_2_1_3_0_And_Target_10_Should_Return_Empty_Array")]
+        [TestCase(10, new[] { 2 }, new int[] { }, TestName = "Test_TwoSum_2_And_Target_10_Should_Return_Empty_Array")]
+        public void Test(int sum, int[] input, int[] output)
+        {
+            var actual = TwoSumSolution.TwoSum(input, sum);
+            var expected = output;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

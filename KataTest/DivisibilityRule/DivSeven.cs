068d342af8f028ad6b2186a372003bcdfa84bf82
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-11 18:45:41 +0800

    Add new kata practice

---
+++ KataTest/DivisibilityRule/DivSeven.cs
@@ -0,0 +1,10 @@
+﻿namespace KataTest.DivisibilityRule
+{
+    public class DivSeven
+    {
+        public int[] Seven(int input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ KataTest/DivisibilityRule/DivisibilityRuleTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace KataTest.DivisibilityRule
+{
+    [TestFixture]
+    public class DivisibilityRuleTests
+    {
+        [TestCase(35, new[] {10, 2})]
+        public void RuleTest(int input, int[] expected)
+        {
+            var divSeven = new DivSeven();
+            Assert.AreEqual(expected,divSeven.Seven(input));
+        }
+            
+    }
+}

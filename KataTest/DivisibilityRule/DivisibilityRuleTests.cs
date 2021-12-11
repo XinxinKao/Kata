b95ca3e9ab80263b4827f94c8b96af6cff465c87
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-11 18:50:55 +0800

    Fix test case type

--- KataTest/DivisibilityRule/DivisibilityRuleTests.cs
+++ KataTest/DivisibilityRule/DivisibilityRuleTests.cs
@@ -5,12 +5,11 @@ namespace KataTest.DivisibilityRule
     [TestFixture]
     public class DivisibilityRuleTests
     {
-        [TestCase(35, new[] {10, 2})]
+        [TestCase(1021, new[] {10, 2})]
         public void RuleTest(int input, int[] expected)
         {
             var divSeven = new DivSeven();
-            Assert.AreEqual(expected,divSeven.Seven(input));
+            Assert.AreEqual(expected, divSeven.Seven(input));
         }
-            
     }
 }

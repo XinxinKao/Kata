7ea33facfa1af42fede87115dbcafb4e9d65f8d7
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-11 22:11:38 +0800

    Finish DivSeven test and code

--- KataTest/DivisibilityRule/DivSeven.cs
+++ KataTest/DivisibilityRule/DivSeven.cs
@@ -1,10 +1,33 @@
 ﻿namespace KataTest.DivisibilityRule
 {
-    public class DivSeven
+    public static class DivSeven
     {
-        public int[] Seven(int input)
+        public static long[] Seven(long input)
         {
-            throw new System.NotImplementedException();
+            var steps = 0;
+
+            while (IsNumberOverTwoDigits(input))
+            {
+                input = GetNumberWithoutLastOne(input) - (GetLastOneNumber(input) * 2);
+                steps++;
+            }
+
+            return new[] {input, steps};
+        }
+
+        private static bool IsNumberOverTwoDigits(long input)
+        {
+            return input > 99;
+        }
+
+        private static long GetLastOneNumber(long stepOneNumber)
+        {
+            return stepOneNumber % 10;
+        }
+
+        private static long GetNumberWithoutLastOne(long numberText)
+        {
+            return numberText / 10;
         }
     }
 }
--- KataTest/DivisibilityRule/DivisibilityRuleTests.cs
+++ KataTest/DivisibilityRule/DivisibilityRuleTests.cs
@@ -6,10 +6,13 @@ namespace KataTest.DivisibilityRule
     public class DivisibilityRuleTests
     {
         [TestCase(1021, new[] {10, 2})]
+        [TestCase(1603, new[] {7, 2})]
+        [TestCase(371, new[] {35, 1})]
+        [TestCase(483, new[] {42, 1})]
+        [TestCase(477557101, new[] {28, 7})]
         public void RuleTest(int input, int[] expected)
         {
-            var divSeven = new DivSeven();
-            Assert.AreEqual(expected, divSeven.Seven(input));
+            Assert.AreEqual(expected, DivSeven.Seven(input));
         }
     }
 }

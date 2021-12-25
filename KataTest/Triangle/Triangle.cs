e2c8c01bc09e640a8f4469b4730cb647d7e5301f
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-25 21:48:24 +0800

    Finish kata.

---
+++ KataTest/Triangle/Triangle.cs
@@ -0,0 +1,15 @@
+﻿namespace KataTest.Triangle
+{
+    public static class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if (a + b > c && b + c > a && a + c > b)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ KataTest/Triangle/TriangleTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace KataTest.Triangle
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [TestCase(5,7,10, true)]
+        [TestCase(-1,2,3, false)]
+        [TestCase(1,-2,3, false)]
+        [TestCase(1,2,-3, false)]
+        [TestCase(1,2,4, false)]
+        [TestCase(4,1,2, false)]
+        [TestCase(1,4,2, false)]
+        [TestCase(1,2,3, false)]
+        [TestCase(3,1,2, false)]
+        [TestCase(1,3,2, false)]
+        [TestCase(3,4,5, true)]
+        public void IsTriangleTest(int a, int b, int c, bool expected)
+        {
+            Assert.AreEqual(expected,Triangle.IsTriangle(a, b, c));
+        }
+    }
+}

b140b92beedf2545b1bfe4b309f98ab2bbf83044
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-25 20:56:45 +0800

    Add new kata. Least Common Multiple

---
+++ KataTest/LeastCommonMultiple/LeastCommonMultiple.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KataTest.LeastCommonMultiple
+{
+    public static class LeastCommonMultiple
+    {
+        public static int Lcm(List<int> nums)
+        {
+            if (nums.Contains(0))
+            {
+                return 0;
+            }
+            if (!nums.Any())
+            {
+                return 1;
+            }
+            
+            var result = 1;
+            
+            foreach (var item in nums)
+            {
+                result = result / GreatestCommonFactor(result, item) * item;
+            }
+            
+            return result;
+        }
+        
+        private static int GreatestCommonFactor(int a, int b)
+        {
+            while (b != 0)
+            {
+                var temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
---
+++ KataTest/LeastCommonMultiple/LeastCommonMultipleTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace KataTest.LeastCommonMultiple
+{
+    [TestFixture]
+    public class LeastCommonMultipleTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(10, LeastCommonMultiple.Lcm(new List<int> {2, 5}));
+            Assert.AreEqual(9, LeastCommonMultiple.Lcm(new List<int> {9}));
+            Assert.AreEqual(12, LeastCommonMultiple.Lcm(new List<int> {2, 3, 4}));
+        }
+    }
+}

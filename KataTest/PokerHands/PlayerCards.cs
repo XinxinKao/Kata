651c02578593f87cac423f6ccda67dd80b5837a5
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-25 21:27:06 +0800

    Refactor - rename field and reformat file layout

--- KataTest/PokerHands/PlayerCards.cs
+++ KataTest/PokerHands/PlayerCards.cs
@@ -5,55 +5,51 @@ namespace KataTest.PokerHands
 {
     public class PlayerCards
     {
+        private readonly Dictionary<CardSuit, string> _cardSuit = new Dictionary<CardSuit, string>()
+        {
+            {CardSuit.C, "C"},
+            {CardSuit.D, "D"},
+            {CardSuit.H, "H"},
+            {CardSuit.S, "S"}
+        };
+
+        private readonly Dictionary<string, int> _cardValue = new Dictionary<string, int>()
+        {
+            {"2", 2},
+            {"3", 3},
+            {"4", 4},
+            {"5", 5},
+            {"6", 6},
+            {"7", 7},
+            {"8", 8},
+            {"9", 9},
+            {"T", 10},
+            {"J", 11},
+            {"Q", 12},
+            {"K", 13},
+            {"A", 14},
+        };
+
+        private string _playerName;
+
         public PlayerCards(string name)
         {
-            PlayerName = name;
+            _playerName = name;
         }
-
-        public string PlayerName { get; set; }
-        public List<CardSuit> Suits { get; set; }
-        public List<string> Values { get; set; }
 
         public void SetCards(string inputCards)
         {
             var cards = inputCards.Split(' ');
-            Values = new List<string>();
-            Suits = new List<CardSuit>();
+            var values = new List<string>();
+            var suits = new List<CardSuit>();
             foreach (var card in cards)
             {
                 var value = card.Substring(0, 1);
                 var suit = card.Substring(1, 1);
 
                 //Values.Add(cardValue.FirstOrDefault(x => x.Key == value).Value);
-                Suits.Add(cardSuit.FirstOrDefault(x => x.Value == suit).Key);
-                
+                suits.Add(_cardSuit.FirstOrDefault(x => x.Value == suit).Key);
             }
         }
-
-
-        private readonly Dictionary<CardSuit, string> cardSuit = new Dictionary<CardSuit, string>()
-        {
-            {CardSuit.C,"C"},
-            {CardSuit.D,"D"},
-            {CardSuit.H,"H"},
-            {CardSuit.S,"S"}
-        };
-
-        private readonly Dictionary<string, int> cardValue = new Dictionary<string, int>()
-        {
-            {"2",2},
-            {"3",3},
-            {"4",4},
-            {"5",5},
-            {"6",6},
-            {"7",7},
-            {"8",8},
-            {"9",9},
-            {"T",10},
-            {"J",11},
-            {"Q",12},
-            {"K",13},
-            {"A",14},
-        };
     }
 }

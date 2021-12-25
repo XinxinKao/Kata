1e89ed0b9dc426c18e94de8e0fc33da2c0c6b1e1
xinyi.kao <xinyi.kao@titansoft.com.sg>
2021-12-25 21:10:04 +0800

    Refactor test. Use testcase to remove duplicate code

--- KataTest/NextBiggerNumber/NextBiggerNumberTests.cs
+++ KataTest/NextBiggerNumber/NextBiggerNumberTests.cs
@@ -5,52 +5,15 @@ namespace KataTest.NextBiggerNumber
     [TestFixture]
     public class NextBiggerNumberTests
     {
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_11_Output_Will_Be_Negative_1()
+        [TestCase(11, -1)]
+        [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
+        [TestCase(414, 441)]
+        [TestCase(144, 414)]
+        public void NextBiggerNumberTest(int num, int expected)
         {
-            var inputNumber = 11;
-            var expected = -1;
-            TestNextBiggerNumber(expected, inputNumber);
-        }
-
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_12_Output_Will_Be_21()
-        {
-            var inputNumber = 12;
-            var expected = 21;
-            TestNextBiggerNumber(expected, inputNumber);
-        }
-
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_513_Output_Will_Be_531()
-        {
-            var inputNumber = 513;
-            var expected = 531;
-            TestNextBiggerNumber(expected, inputNumber);
-        }
-
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_2017_Output_Will_Be_2071()
-        {
-            var inputNumber = 2017;
-            var expected = 2071;
-            TestNextBiggerNumber(expected, inputNumber);
-        }
-
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_414_Output_Will_Be_441()
-        {
-            var inputNumber = 414;
-            var expected = 441;
-            TestNextBiggerNumber(expected, inputNumber);
-        }
-
-        [Test]
-        public void TestNextBiggerNumber_When_Input_Is_144_Output_Will_Be_414()
-        {
-            var inputNumber = 144;
-            var expected = 414;
-            TestNextBiggerNumber(expected, inputNumber);
+            TestNextBiggerNumber(expected, num);
         }
 
         private void TestNextBiggerNumber(long expected, long inputNumber)

5aaaecaa2bb36d957266a8f4fd8666450a95cc71
xinyi.kao <xinyi.kao@titansoft.com.sg>
2022-02-04 22:24:17 +0800

    Add new kata - Reversed sequence

---
+++ KataTest/ReversedSequence/ReversedSequence.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace KataTest.ReversedSequence
+{
+    public static class ReversedSequence
+    {
+        public static IEnumerable<int> ReverseSeq(int n)
+        {
+            for (var i = n; i > 0; i--)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ KataTest/ReversedSequence/SolutionTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace KataTest.ReversedSequence
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(ReversedSequence.ReverseSeq(5), Is.EqualTo(new int[] {5, 4, 3, 2, 1}));
+        }
+    }
+}
